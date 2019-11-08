6b82591b556bfc7a8ad6acf80dcfaeb29e031b94
pol_afreitas <andre.freitas@polarising.com>
2019-11-08 14:07:40 +0000

    initial
    Change-Id: I8ea156d79e79703cb50d667a288f881d777f6b99

---
+++ src/main/java/com/example/demobatchcassandra/DemoBatchCassandraApplication.java
@@ -0,0 +1,13 @@
+package com.example.demobatchcassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBatchCassandraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBatchCassandraApplication.class, args);
+	}
+
+}
